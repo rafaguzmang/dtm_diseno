a786a3bd4b26a57ebbe0d78772b83073c41e1647
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-01-09 15:37:55 -0600

    Se agrega consulta del material del modulo materiales Se agrega calibre, ancho y largo en  decimales Se agrega el campo area e id

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,11 @@
     "name":"Diseño",
     "data":[
         'security/ir.model.access.csv',
-        'views/dtm_diseno_views.xml'
+        'views/menu_item.xml',
+        'views/dtm_diseno_views.xml',
+        'views/dtm_diseno_realizados_views.xml',
+        'views/dtm_diseno_materiales_views.xml',
+
     ],
     "depends" : ["base"]
 }
--- models/dtm_diseno.py
+++ models/dtm_diseno.py
@@ -9,6 +9,49 @@ class DisenoMateriales(models.Model):
 
     @api.onchange("sheets")
     def _onchange_apartado(self):
-        print(self.material_id.id)
+        if self.material_id.id:
+            self.env.cr.execute("UPDATE dtm_materiales SET apartado = "+ str(self.sheets) +" WHERE id = " + str(self.material_id.id))
 
-        self.env.cr.execute("UPDATE dtm_materiales SET apartado = "+ str(self.sheets) +" WHERE id = " + str(self.material_id.id) )
+
+class Realizados(models.Model):
+    _name = "dtm.diseno.realizados"
+    _description = "Lleva el listado de todo el material cortado en la Laser"
+
+    drawingname  = fields.Char(string="DRAWINGNAME")
+    sheets = fields.Char(string="SHEETS")
+    material_id = fields.Char(string="MATERIALES")
+
+
+class Materiales(models.Model):
+    _name = "dtm.diseno.materiales"
+    _description = "Carga la tabla de materiales del Modulo dtm_materiales para uso del diseñador"
+
+    material = fields.Char(string="MATERIAL")
+    calibre = fields.Char(string="CALIBRE")
+    largo = fields.Char(string="LARGO")
+    ancho = fields.Char(string="ANCHO")
+    area = fields.Char(string="AREA")
+    cantidad = fields.Char(string="STOCK")
+    apartado = fields.Char(string="APARTADO")
+    disponible = fields.Char(string="DISPONIBLE")
+
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super(Materiales,self).get_view(view_id, view_type,**options)
+
+        get_info = self.env['dtm.materiales'].search([])
+
+        self.env.cr.execute("DELETE FROM dtm_diseno_materiales ")
+        for result in get_info:
+            id = str(result.id)
+            material = str(result.material_id.nombre)
+            calibre = str(result.calibre)
+            largo = str(result.largo)
+            ancho = str(result.ancho)
+            area = str(result.area)
+            disponible = str(result.disponible)
+            self.env.cr.execute("INSERT INTO dtm_diseno_materiales (id, material, calibre, largo, ancho, area, disponible)" +
+                                "VALUES ("+id+",'"+ material + "','" + calibre + "','" + largo + "','" + ancho + "','" + area +  "','" + disponible +"')")
+        return res
+
+
+
