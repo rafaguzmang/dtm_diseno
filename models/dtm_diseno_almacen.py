e7b79ef925d02a6f36940b2e62149dfb7160edfa
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-08-31 13:34:33 -0600

    Se modifica el almacén para las capturas y consultas de los items para los materiales faltantes

--- models/dtm_diseno_almacen.py
+++ models/dtm_diseno_almacen.py
@@ -85,14 +85,23 @@ class Materiales(models.Model):
     def get_view(self, view_id=None, view_type='form', **options):#Carga los items de todos los módulos de Almacén en un solo módulo de diseño
         res = super(Materiales,self).get_view(view_id, view_type,**options)
         get_almacen = self.env['dtm.diseno.almacen'].search([])
+        modelos = ['dtm.materiales',"dtm.materiales.angulos","dtm.materiales.solera","dtm.materiales.rodamientos","dtm.materiales.pintura","dtm.materiales.perfiles","dtm.materiales.otros","dtm.materiales.maquinados","dtm.materiales.canal","dtm.materiales.tornillos","dtm.materiales.tubos","dtm.materiales.varilla"]
+
         for item in get_almacen:
-            get_lamina = self.env['dtm.materiales'].search([("codigo","=",item.id)])
-            if get_lamina:
-                item.write({
-                    "cantidad": get_lamina.cantidad,
-                    "apartado": get_lamina.apartado,
-                    "disponible": get_lamina.disponible,
-                })
+            item.write({
+                "cantidad":0,
+                "apartado":0,
+                "disponible":0,
+            })
+            for modelo in modelos:
+                get_lamina = self.env[modelo].search([("codigo","=",item.id)])
+                if get_lamina:
+                    print(get_lamina,get_lamina.codigo)
+                    item.write({
+                        "cantidad": get_lamina.cantidad,
+                        "apartado": get_lamina.apartado,
+                        "disponible": get_lamina.disponible,
+                    })
 
         for find_id in range(1,self.env['dtm.diseno.almacen'].search([], order='id desc', limit=1).id+2):
                 if not self.env['dtm.diseno.almacen'].search([("id","=",find_id)]):
